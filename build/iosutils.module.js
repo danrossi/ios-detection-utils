aec46d6e10aa74a12e40a6fed9d644d23efc6e0d
Daniel Rossi <electroteque@protonmail.com>
2024-04-06 08:54:48 +1100

    screen dimension check change. Ipad width sometimes reported more than visionOS and do a height check also.

--- build/iosutils.module.js
+++ build/iosutils.module.js
@@ -23,15 +23,17 @@
  * @author danrossi / https://github.com/danrossi
  */
 
-let visionOSWidth = 1306;
+let visionOSWidth = 1306,
+visionOSHeight = 735;
 
 class IOSUtils {
     /**
      * Only way to detect visionOS is detect for MacIntel Ipad and screen width greater than 1306
      * xr detection for visionOS is only possible if the flag is enabled.
+     * Need to use width instead of availWidth as Ipad is more, and do a height check as Ipad height is more.
      */
     static get isVisionOS() {
-        return this.isIpad && screen.availWidth >= visionOSWidth; 
+        return this.isIpad && screen.width >= visionOSWidth && screen.height <= visionOSHeight; 
     }
 
     /**
@@ -44,9 +46,11 @@ class IOSUtils {
     /**
      * Iphone and Ipad detection and filter for visionOS screens. 
      * We don't want to detect visionOS as iOS to prevent including mobile features like orientation controls.
+     * Need to use width instead of availWidth as Ipad is more, and do a height check as Ipad height is more.
+     * 
      */
     static get isIOS() {
-        return ((/iP(hone|ad)/i).test(navigator.platform) || this.isIpad && screen.availWidth < visionOSWidth);
+        return ((/iP(hone|ad)/i).test(navigator.platform) || (this.isIpad && screen.width < visionOSWidth && screen.height > visionOSHeight));
     }
 
     static requireOrientationPermission() {
